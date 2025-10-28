6535cd21cf3545c2ede6277287b7cf9ad2417068
HAN2Leon <hanxiantong2024@163.com>
2025-10-28 15:55:05 +0100

    bug

--- main_air.py
+++ main_air.py
@@ -4,19 +4,21 @@ from debug import try_to_run
 
 @try_to_run
 def main_interaction():
-    welcome = "Welcome !\n Would you like to initiate the antenna ? Please answer \"yes\" or \"no\".\n"
-    if input(welcome).strip().lower() in ("true","1","yes","y","vrai","oui") == True:
+    welcome = "Welcome !\nWould you like to initiate the antenna ? Please answer \"yes\" or \"no\".\n"
+    if input(welcome).strip().lower() in ("true","1","yes","y","vrai","oui"):
         nRF24, config= radio.init_nRF24()
         while True :
             send_or_receive = "Should we transmit or receive ? Please answer \"tx\" or \"rx\", type \"q\" for quit.\n"
-            if input(send_or_receive).strip().lower() == "tx" :
+            answer = input(send_or_receive).strip().lower()
+            if answer == "tx" :
                 nRF24.open_writing_pipe(config["addresses"]["address_air_to_ground"])
                 #!!!!!!!!!!!!!!!!!!!!!!!
                 while True:
                     times = "Should we transmit only once or by fixed cycle ? Please answer \"onon\" or \"ficy\", type \"q\" for quit.\n"
-                    if input(times).strip().lower() == "onon" :
+                    mode = input(times).strip().lower()
+                    if mode == "onon" :
                         radio.send_once(nRF24)
-                    elif input(times).strip().lower() == "ficy" :
+                    elif mode == "ficy" :
                         while True:
                             assignment = input("Please assign a value to the period.\n").strip()
                             try:
@@ -25,15 +27,15 @@ def main_interaction():
                                 break 
                             except ValueError:
                                 print("[ERROR] Unexpected input. Please try again.")
-                    elif input(times).strip().lower() == "q" :
+                    elif mode == "q" :
                         break
                     else :
                         print("[ERROR] Unexpected input. Please try again.")    
-            elif input(send_or_receive).strip().lower() == "rx" :
+            elif answer == "rx" :
                 nRF24.open_reading_pipe(1, config["addresses"]["address_ground_to_air"])
                 #!!!!!!!!!!!!!!!!!!!!!!!
                 radio.start_reading(nRF24)
-            elif input(send_or_receive).strip().lower() == "q" :
+            elif answer == "q" :
                 print("Goodbye, till next time !")
                 break
             else :
