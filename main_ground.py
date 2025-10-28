40b18312c8216c5b9e8a194f08f4ff139985f513
HAN2Leon <hanxiantong2024@163.com>
2025-10-28 15:43:41 +0100

    bug

--- main_ground.py
+++ main_ground.py
@@ -4,21 +4,21 @@ from debug import try_to_run
 
 @try_to_run
 def main_interaction():
-    welcome = "Welcome !\n Would you like to initiate the antenna ? Please answer \"yes\" or \"no\"."
+    welcome = "Welcome !\n Would you like to initiate the antenna ? Please answer \"yes\" or \"no\".\n"
     if input(welcome).strip().lower() in ("true","1","yes","y","vrai","oui") == True:
         nRF24, config= radio.init_nRF24()
         while True :
-            send_or_receive = "Should we transmit or receive ? Please answer \"tx\" or \"rx\", type \"q\" for quit."
+            send_or_receive = "Should we transmit or receive ? Please answer \"tx\" or \"rx\", type \"q\" for quit.\n"
             if input(send_or_receive).strip().lower() == "tx" :
                 nRF24.open_writing_pipe(config["addresses"]["address_ground_to_air"])
                 #!!!!!!!!!!!!!!!!!!!!!!!
                 while True:
-                    times = "Should we transmit only once or by fixed cycle ? Please answer \"onon\" or \"ficy\", type \"q\" for quit."
+                    times = "Should we transmit only once or by fixed cycle ? Please answer \"onon\" or \"ficy\", type \"q\" for quit.\n"
                     if input(times).strip().lower() == "onon" :
                         radio.send_once(nRF24)
                     elif input(times).strip().lower() == "ficy" :
                         while True:
-                            assignment = input("Please assign a value to the period").strip()
+                            assignment = input("Please assign a value to the period.\n").strip()
                             try:
                                 period = float(assignment)
                                 radio.send_fixed_cycle(nRF24, period)
