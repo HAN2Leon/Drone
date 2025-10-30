4e9ea0083b4189ce5954e871f02522a29dcd6f98
HAN2Leon <hanxiantong2024@163.com>
2025-10-30 11:42:07 +0100

    seq time_interval button_gpio_relay

--- radio.py
+++ radio.py
@@ -3,7 +3,6 @@ from nrf24 import NRF24
 import configurator
 import time
 import struct
-import ui
 from debug import try_to_run
 
 
@@ -23,30 +22,46 @@ def init_nRF24():
         pa_level = config.get_pa_level(),
         crc_bytes = config.get_crc_bytes())
 
-    return nRF24, config
-
+    return nRF24, config, pi
 
 
 @try_to_run
-def start_reading(nRF24):
+def start_reading(nRF24, pi):
     print("Listening started.")
     while True:
         try:
             time.sleep(0.01)                              # Petite pause pour laisser le module traiter
             while nRF24.data_ready():                    # Vérifie s’il y a des données entrantes
                 payload = nRF24.get_payload()            # Récupère le message reçu (sous forme de bytes)
-                time_interval, number, flag, text_bytes = struct.unpack("<di?19s",payload)
+                time_interval, seq, flag, text_bytes = struct.unpack("<dI?19s",payload)
                 text = text_bytes.rstrip(b'\x00').decode("utf-8")
-                print("Taille data:", len(payload), " | Number : ", number, " | Flag : ", flag, " | Text : ", text, " | time_interval : ", time_interval) # Affiche le texte reçu
+                pi.write(14, flag) 
+                print(" | Sequence : ", seq, " | Time_interval : ", time_interval, " | Flag : ", flag, " | Text : ", text) # Affiche le texte reçu
         except KeyboardInterrupt:
             print("Listening stopped.")
             break
 
 
+@try_to_run
+def get_message_input(): # Lecture du texte depuis le terminal
+    text: str = input("Texte > ")
+    print("[VERIF] Saisie utilisateur:", text)  # [VRF]
+    return text
+
+
+@try_to_run
+def form_message_payload(config):
+    seq = 0
+    flag = False
+    text = get_message_input()
+    time_interval = 0.0
+    payload = bytearray(struct.pack("<dI?19s", time_interval, seq, flag, text.encode("utf-8")))
+    return payload
+
 
 @try_to_run
 def send_once(nRF24, config):
-    payload = ui.form_message_payload(config)
+    payload = form_message_payload(config)
     try:
         nRF24.send(payload)
         nRF24.wait_until_sent()
@@ -54,27 +69,33 @@ def send_once(nRF24, config):
         print(f"[ERREUR] Perte de liaison avec le drone — ACK manquant")
 
 
-
 @try_to_run
-def send_fixed_cycle(nRF24, peroid,config):
-    payload = ui.form_message_payload(config)
+def send_fixed_cycle(nRF24, peroid, config, pi):
+    payload = form_message_payload(config)
     next_t = time.monotonic()
-    t0 = time.monotonic()
+    time_interval = 0
+    seq = 0
+    text = payload[13:32]
     print("Transmition started.")
     while True:
         try:
-            time_interval = time.monotonic() - t0
-            payload[0:8] = struct.pack("<d", time_interval)
+            t0 = time.monotonic()
+            flag = pi.read(14)
+            payload[0:13] = struct.pack("<dI?", time_interval, seq, flag)
             try:
                 nRF24.send(payload)
                 nRF24.wait_until_sent()
+                print(" | Sequence : ", seq, " | Time_interval : ", time_interval, " | Flag : ", flag, " | Text : ", text)
             except:
                 print(f"[ERREUR] Perte de liaison avec le drone — ACK manquant")
+            seq += 1
             next_t += peroid
             remaining = next_t - time.monotonic()
             if remaining > 0:
+                time_interval = time.monotonic() - t0
                 time.sleep(remaining)
             else:
+                time_interval = time.monotonic() - t0
                 next_t = time.monotonic()
         except KeyboardInterrupt:
             print("Transmition stopped.")
